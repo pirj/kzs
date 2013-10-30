deacaae1289658e7176e6e1d0d05b8460a9d91b4
Viacheslav Bobrov <babrovka@gmail.com>
2013-10-30 16:46:12 +0400

    permits create

--- app/assets/javascripts/permits.js
+++ app/assets/javascripts/permits.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
-
     $("#permit_start_date, #permit_expiration_date").datepicker({ dateFormat: "dd-mm-yy" });
 
-
+	$('#permit_permit_type').on("change", function() {
+		  if ($('#permit_permit_type option:selected').val() == "user") {
+			$('#permit_user_id').show();
+			$('#permit_vehicle_id').hide();
+		  } else {
+		    $('#permit_user_id').hide();
+			$('#permit_vehicle_id').show();
+		  }	
+	});
+	
+	
 });
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,4 +2,8 @@ class Vehicle < ActiveRecord::Base
   attr_accessible :brand, :model, :register_document, :vehicle_body, :register_sn
   
   WORK_STATUSES = %w[passenger_car truck special_car]
+  
+  def vehicle_title
+    "#{register_sn} #{brand} #{model}"
+  end
 end
