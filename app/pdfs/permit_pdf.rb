2132553ce8f043ca4cd2a79a0cbb1af58894f924
Viacheslav Bobrov <babrovka@gmail.com>
2013-10-31 15:12:48 +0400

    pdf layout

--- app/pdfs/permit_pdf.rb
+++ app/pdfs/permit_pdf.rb
@@ -7,20 +7,21 @@ class PermitPdf < Prawn::Document
   end
   
   def initialize(permit, view)
-    super(top_margin: 30)
+    super(margin: 0, :page_size => [595, 420])
     @permit = permit
     @view = view
     russian_font
-    text "#{@permit.number}", :size => 15
-    text "#{I18n.t(@permit.permit_type)}", :size => 15
-    if @permit.permit_type == 'vehicle'
-    text "#{@permit.vehicle.vehicle_title}", :size => 15
-    else
-    @user = User.find(@permit.user_id).first_name_with_last_name
-    text "#{@user}", :size => 15
-    end
-    text "#{I18n.t(@permit.permit_class)}", :size => 15
-    text "#{@permit.start_date.strftime('%d.%m.%y')} - #{@permit.expiration_date.strftime('%d.%m.%y')}", :size => 15 
+
+    float {image "#{Rails.root}/app/assets/images/propusk_back.jpg"}
+
+    move_down 140
+    
+    draw_text "№#{@permit.number}", :size => 25, :at => [484,262]
+    
+    draw_text "#{@permit.vehicle.register_sn}", :size => 45, :at => [40,110]
+
+    # text "#{@permit.vehicle.vehicle_title}", :size => 15
+    # text "#{@permit.start_date.strftime('%d.%m.%y')} - #{@permit.expiration_date.strftime('%d.%m.%y')}", :size => 15
   end
   
 
@@ -30,7 +31,9 @@ class PermitPdf < Prawn::Document
       "Verdana" => {
         :bold => "#{Rails.root}/app/assets/fonts/verdanab.ttf",
         :italic => "#{Rails.root}/app/assets/fonts/verdanai.ttf",
-        :normal  => "#{Rails.root}/app/assets/fonts/verdana.ttf" })
+        :normal  => "#{Rails.root}/app/assets/fonts/verdana.ttf" },
+      "RoadNumbers" => {
+            :normal  => "#{Rails.root}/app/assets/fonts/RoadNumbers2.0.ttf" })
     font "Verdana"
   end
 end
