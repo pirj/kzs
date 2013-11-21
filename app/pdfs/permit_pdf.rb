bec8075855823c2944e24f4785a429a97fadc6bb
Viacheslav Bobrov <babrovka@gmail.com>
2013-11-21 13:12:47 +0400

    personal permit fields

--- app/pdfs/permit_pdf.rb
+++ app/pdfs/permit_pdf.rb
@@ -21,15 +21,29 @@ class PermitPdf < Prawn::Document
       font "Verdana"
       
       draw_text "#{@organization.title}", :size => 9, :at => [90,147]
-      draw_text "#{@permit.number}", :size => 9, :at => [132,134]
       
-      draw_text "#{@user.last_name}", :size => 9, :at => [100,120]
-      draw_text "#{@user.first_name}", :size => 9, :at => [100,105]
-      draw_text "#{@user.middle_name}", :size => 9, :at => [100,92]
+      draw_text "Пропуск №", :size => 9, :at => [90,134]
+      draw_text "#{@permit.number}", :size => 9, :at => [147,134]
       
-      draw_text "#{@user.position}", :size => 9, :at => [135,78]
-      draw_text "#{@organization.title}", :size => 9, :at => [142,50]
-      draw_text "#{@permit.expiration_date.strftime('%d.%m.%y')}", :size => 9, :at => [80,5]
+      draw_text "Ф.", :size => 9, :at => [90,120]
+      draw_text "#{@user.last_name}", :size => 9, :at => [105,120]
+      
+      draw_text "И.", :size => 9, :at => [90,105]
+      draw_text "#{@user.first_name}", :size => 9, :at => [105,105]
+      
+      draw_text "О.", :size => 9, :at => [90,92]
+      draw_text "#{@user.middle_name}", :size => 9, :at => [105,92]
+      
+      draw_text "Должность:", :size => 9, :at => [90,78]
+      draw_text "#{@user.position}", :size => 9, :at => [150,78]
+      
+      draw_text "Организация:", :size => 9, :at => [90,50]
+      draw_text "#{@organization.title}", :size => 9, :at => [160,50]
+      
+      draw_text "Подпись:", :size => 9, :at => [6,21]
+      
+      draw_text "Действителен до:", :size => 9, :at => [6,6]
+      draw_text "#{@permit.expiration_date.strftime('%d.%m.%y')}", :size => 9, :at => [92,5]
 
       
     else
