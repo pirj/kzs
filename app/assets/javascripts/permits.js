de8779297bf1146e01875b248a037def5cb4277e
Viacheslav Bobrov <babrovka@gmail.com>
2013-11-21 13:27:06 +0400

    walker permit

--- app/assets/javascripts/permits.js
+++ app/assets/javascripts/permits.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+	
+	  if ($('#permit_permit_type_vehicle').attr('checked') == "checked"){
+		  $('#permit_user_fields').hide();
+			$('#permit_vehicle_fields').show();
+			$('#permit_user_id').val('');
+			$('#permit_drivers').chosen({max_selected_options: 3}); 
+		}
+		else {
+		  $('#permit_user_fields').show();
+			$('#permit_vehicle_fields').hide();
+		}
+	
     $("#permit_start_date, #permit_expiration_date, #permit_date").datepicker({ dateFormat: "dd-mm-yy" });
 	
 	$('#user_type').on("click", function() {
