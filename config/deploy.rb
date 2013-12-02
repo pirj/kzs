d7f20000120cd67444de0da7f33c1a461d98fb64
Viacheslav Bobrov <babrovka@gmail.com>
2013-12-02 14:57:58 +0400

    deploy multiple

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,63 @@
+require 'rvm/capistrano'
+require 'bundler/capistrano'
+load 'config/recipes/base'
+load 'config/recipes/monit'
+
 load 'deploy/assets'
 
-set :stages, %w(production staging)
-set :default_stage, "staging"
-require 'capistrano/ext/multistage'
+server "5.178.80.26", :web, :app, :db, primary: true
 
+set :user, "user"
 set :application, "kzs"
+set :deploy_to, "/home/user/projects/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
 
+set :scm, "git"
+set :repository, "git@github.com:babrovka/kzs.git"
+set :branch, "master"
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
 
 
+task :copy_database_config do
+   db_config = "#{shared_path}/database.yml"
+   run "cp #{db_config} #{latest_release}/config/database.yml"
+end
 
+namespace :deploy do
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
+end
+
+namespace(:thin) do
+  task :stop do
+    run "thin stop -C /etc/thin/kzs.yml"
+   end
+  
+  task :start do
+    run "thin start -C /etc/thin/kzs.yml"
+  end
+
+  task :restart do
+    run "thin restart -C /etc/thin/kzs.yml"
+  end
+end
+
+namespace :deploy do
+  task :setup_solr_data_dir do
+    run "mkdir -p #{shared_path}/solr/data"
+  end
+end
+
+before "deploy:assets:precompile", "copy_database_config"
+after "deploy", "deploy:cleanup"
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
-require 'rvm/capistrano'
-require 'bundler/capistrano'
+require "rvm/capistrano"
+require "bundler/capistrano"
 load 'config/recipes/base'
 
 load 'deploy/assets'
