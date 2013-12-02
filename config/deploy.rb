d738b75944239561a23ba76ce821eeed7e58cbe1
Viacheslav Bobrov <babrovka@gmail.com>
2013-12-02 14:46:33 +0400

    deploy multiple

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :stages, %w(production staging)
 set :default_stage, "staging"
 require 'capistrano/ext/multistage'
 
-
+set :application, "kzs"
 set :deploy_via, :remote_cache
 set :use_sudo, false
 
