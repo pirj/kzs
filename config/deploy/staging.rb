31390ccf159a681195d7461d260332fda3309692
Viacheslav Bobrov <babrovka@gmail.com>
2013-12-02 14:45:36 +0400

    deploy multiple

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,19 +3,11 @@ require 'bundler/capistrano'
 load 'config/recipes/base'
 load 'config/recipes/monit'
 
-load 'deploy/assets'
+
 
 server "5.178.80.26", :web, :app, :db, primary: true
 
-set :user, "user"
-set :application, "kzs"
-set :deploy_to, "/home/user/projects/#{application}"
-set :deploy_via, :remote_cache
-set :use_sudo, false
 
-set :scm, "git"
-set :repository, "git@github.com:babrovka/kzs.git"
-set :branch, "master"
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
