448d15e6c65eec862803b381c29393f45786111c
Viacheslav Bobrov <babrovka@gmail.com>
2013-12-06 17:38:23 +0400

    tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,8 @@
 class TasksController < ApplicationController
   def execute
+    @task = Task.find(params[:id])
+    @task.completed = true
+    @task.save!
+    redirect_to :back, :notice => 'task_completed'
   end
 end
