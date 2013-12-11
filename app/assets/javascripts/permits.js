046599981cfe75119f6d263636b615489260526b
artem sanzharevskiy <artemini@Mac-mini-artem-2.local>
2013-12-11 15:44:36 +0400

    permits type js

--- app/assets/javascripts/permits.js
+++ app/assets/javascripts/permits.js
@@ -1,95 +1,77 @@
 $(document).ready(function(){
-	/*
-	if ($('#permit_permit_type_vehicle').attr('checked') == "checked"){             //logic tabs
-	  $('#permit_user_fields').hide();
-		$('#permit_vehicle_fields').show();
-		$('#temporary_permit_fields').hide();
-		$('#permit_user_id').val('');
-		$('#permit_drivers').chosen({max_selected_options: 3}); 
-	}
-	else if ($('#permit_permit_type_user').attr('checked') == "checked"){
-	  $('#permit_user_fields').show();
-		$('#permit_vehicle_fields').hide();
-		$('#temporary_permit_fields').hide();
-	}
-	else if ($('#permit_permit_type_daily').attr('checked') == "checked"){
-	  $('#permit_user_fields').hide();
-		$('#permit_vehicle_fields').hide();
-		$('#temporary_permit_fields').show();
-	}
-*/
+
+    $('#permit_way_bill').on("change", function() {                 // putev list only transport
+        $('#permit_drivers_chosen').toggle();
+    });
+
+    $("#permit_start_date, #permit_expiration_date, #permit_date").datepicker({ dateFormat: "dd-mm-yy" });
+
+    var permit = {
+
+        setType: function (type) {
+          //  console.log(type);
+            document.location.hash = type;
+
+            switch(type) {
+                case '#user':
+                    $('#permit_user_fields').show();
+                    $('#permit_vehicle_fields').hide();
+                    $('#date_fields').show();
+                    $('#temporary_permit_fields').hide();
+                    break;
+
+                case '#vehicle':
+                    $('#permit_user_fields').hide();
+                    $('#permit_vehicle_fields').show();
+                    $('#date_fields').show();
+                    $('#temporary_permit_fields').hide();
+                    $('#permit_drivers').chosen({max_selected_options: 3});
+
+                    break;
+                case '#daily':
+                    $('#permit_user_fields').hide();
+                    $('#permit_vehicle_fields').hide();
+                    $('#date_fields').hide();                     //ne srabotalo
+                    $('#temporary_permit_fields').show();
+                    break;
 
 
+            }
+        }
 
-  $("#permit_start_date, #permit_expiration_date, #permit_date").datepicker({ dateFormat: "dd-mm-yy" });
-	
+    };
+
 	$('#user_type').on("click", function() {
-	  $('#permit_user_fields').show();
-		$('#permit_vehicle_fields').hide();
-		$('#date_fields').show();
-		$('#temporary_permit_fields').hide();
-
-
-		/*inputs = $('#permit_vehicle_fields :input, #permit_temporary_fields :input')
-		inputs.each(function() {
-		  $(this).removeAttr('value');
-		});*/
-
-
-
-
+        permit.setType('#user');
 	});
 	
 	$('#vehicle_type').on("click", function() {
-	  $('#permit_user_fields').hide();
-		$('#permit_vehicle_fields').show();
-		$('#date_fields').show();
-		$('#temporary_permit_fields').hide();
-
-//		$('#permit_user_id').val('');
-
-		$('#permit_drivers').chosen({max_selected_options: 3});
-
-		/*inputs = $('#permit_user_fields :input, #permit_temporary_fields :input')
-		inputs.each(function() {
-		  $(this).removeAttr('value');
-		});*/
+        permit.setType('#vehicle');
 	});
 	
 	$('#daily_pass').on("click", function() {
-	  $('#permit_user_fields').hide();
-	$('#permit_vehicle_fields').hide();
-	$('#date_fields').hide();                     //ne srabotalo
-		$('#temporary_permit_fields').show();
-
-
-
-        /*	$('#permit_vehicle_vehicle_body, #permit_vehicle_brand').val('');
-
-
-		inputs = $('#permit_vehicle_fields :input, #permit_temporary_fields :input')
-		inputs.each(function() {
-		  $(this).removeAttr('value');
-		});*/
+        permit.setType('#daily');
 	});
 
-	
-	// $('#temporary').on("click", function() {
-	//     $('#permanent_fields').hide();
-	// 	$('#permit_start_date, #permit_expiration_date').val('');
-	// 	$('#temoporary_fields').show();
-	// });
-	// 
-	// 
-	// $('#permanent').on("click", function() {
-	// 	$('#permanent_fields').show();
-	// 	$('#temoporary_fields').hide();
-	// 	$('#permit_date').val('');
-	// });
-	
-	$('#permit_way_bill').on("change", function() {                 // putev list
-		$('#permit_drivers_chosen').toggle();
-	});
-	
+
+    (function initialization(type){
+
+        if (type == "") {
+         //   permit.setType('#user');
+            $('#user_type').click();
+        }
+        else if (type == "#user")  {
+          //  permit.setType(type);
+
+            $('#user_type').click();
+        }
+        else if (type == "#vehicle"){
+            $('#vehicle_type').click();
+        }
+        else if (type == "#daily"){
+            $('#daily_pass').click();
+        };
+
+    })(document.location.hash);
 	
 });
