afa90cc2e65b99182be0a15ee8b6bbed055bad9a
Tagir <artemini@Mac-mini-artem-2.local>
2013-12-18 14:54:10 +0400

    temp fix bootstrap.css

---
+++ app/assets/javascripts/sidebar.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+
+
+
+    $('.menu-dir div.menu-link').click(function () {
+        var childDir = $(this).next();
+
+        childDir.toggleClass('open');
+
+
+
+        var counter =  childDir.children('.menu-link').length;
+        console.log(counter);
+
+        childDir.css({'height': (counter*50)+'px'});
+
+        $('.l-page__column').mouseleave(function() {
+            childDir.removeClass('open');
+        });
+    });
+
+});
