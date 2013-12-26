5eb7356044cb9b1218a4b1e4e0fba3f905ccd8db
Viacheslav Bobrov <babrovka@gmail.com>
2013-12-26 12:45:31 +0400

    permits table ajax

--- app/controllers/permits_controller.rb
+++ app/controllers/permits_controller.rb
@@ -12,6 +12,7 @@ class PermitsController < ApplicationController
     
     @permits = Permit.order(sort_type)
     
+    @controller = params[:controller]
     
     
     if params[:scope] == "expired"
@@ -67,6 +68,7 @@ class PermitsController < ApplicationController
     @daily_pass = @permit.daily_pass
     respond_to do |format|
       format.html
+      format.js
       format.pdf do
         pdf = PermitPdf.new(@permit, view_context)
         send_data pdf.render, filename: "permit#{@permit.id}.pdf",
