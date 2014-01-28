f221305196738a0e506ebae99bc95ae2e5ba16db
Viacheslav Bobrov <babrovka@gmail.com>
2014-01-28 14:17:04 +0400

    comment out precompile recipe

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,21 +43,21 @@ task :staging do
   #   end
   # end
   
-  namespace :deploy do
-    namespace :assets do
-      desc 'Run the precompile task locally and rsync with shared'
-      task :precompile, :roles => :web, :except => { :no_release => true } do
-        from = source.next_revision(current_revision)
-        if releases.length <= 1 || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-          %x{bundle exec rake assets:precompile}
-          %x{rsync --recursive --times --rsh=ssh --compress --human-readable --progress public/assets #{user}@#{host}:#{shared_path}}
-          %x{bundle exec rake assets:clean}
-        else
-          logger.info 'Skipping asset pre-compilation because there were no asset changes'
-        end
-      end
-    end
-  end
+  # namespace :deploy do
+  #   namespace :assets do
+  #     desc 'Run the precompile task locally and rsync with shared'
+  #     task :precompile, :roles => :web, :except => { :no_release => true } do
+  #       from = source.next_revision(current_revision)
+  #       if releases.length <= 1 || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+  #         %x{bundle exec rake assets:precompile}
+  #         %x{rsync --recursive --times --rsh=ssh --compress --human-readable --progress public/assets #{user}@#{host}:#{shared_path}}
+  #         %x{bundle exec rake assets:clean}
+  #       else
+  #         logger.info 'Skipping asset pre-compilation because there were no asset changes'
+  #       end
+  #     end
+  #   end
+  # end
 
   namespace(:thin) do
     task :stop do
