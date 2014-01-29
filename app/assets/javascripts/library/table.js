59ef94bbcf1e1c5f043b1f0758d8241039b7b4e9
Tagir <tagirshine@yandex.ru>
2014-01-29 20:37:05 +0400

    permit edit

--- app/assets/javascripts/library/table.js
+++ app/assets/javascripts/library/table.js
@@ -4,67 +4,49 @@
 $(document).ready(function(){
 
         var actions = {
-            open: function(){},
+            append: function(elem, target){
+                target.toggle();
+                elem.addClass('tr-slim')
+            },
+            replace: function(elem, target){
+                elem.toggle();
+                target.toggle();
+            },
             close: function(table){
                 table.find('tr').show();
                 table.find('.js-more-info').hide();
-
+                table.find('tr.tr-slim').removeClass('tr-slim');
             }
         }
 
         $(document).on('click', '.js-row-clickable tr', function(e) {
 
-        //    console.log('tr');
             var $elem, $target, id;
             e.preventDefault();
             $elem = $(this);
 
             $thisTable = $((this.parentNode).parentNode);
 
-
-
-
-
-
-
             id = $elem.data('id');
             $target = $elem.siblings(".js-more-info[data-id=" + id + "]");
-
-
-
-
-
 
             if (!$elem.hasClass('js-more-info')) {actions.close($thisTable);}
 
             if ((id != null) && $target.length > 0) {               //существует ссылка и связанный обьект
-
                 _.each((this.parentNode).parentNode.classList, function(a){
-
-
 
                     switch (a) {
 
                         case 'm-table-append':
 
-                           $target.toggle();
+                            actions.append($elem, $target);
                         break
 
                         case 'm-table-replacement':
-                            $elem.toggle();
-                            $target.toggle();
+                            actions.replace($elem, $target);
                         break
-
                     }
                 })
-
-
             }
-
-
-
-
-
-
         });
 });
