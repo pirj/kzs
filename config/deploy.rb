58535e762e9c22f0f62d34f5223ac983074f6ce7
Viacheslav Bobrov <babrovka@gmail.com>
2014-02-05 13:56:50 +0400

    add mercury server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 require 'bundler/capistrano'
 load 'config/recipes/base'
 load 'deploy/assets'
-require 'rvm/capistrano'
+# require 'rvm/capistrano'
 
 task :staging do
   
@@ -129,6 +129,45 @@ task :production do
   #     run "service uwsgi stop"
   #   end
   # end
+
+  after "deploy", "deploy:cleanup"
+end
+
+
+task :mercury do
+  module UseScpForDeployment
+    def self.included(base)
+      base.send(:alias_method, :old_upload, :upload)
+      base.send(:alias_method, :upload,     :new_upload)
+    end
+  
+    def new_upload(from, to, options = {}, &block)
+    old_upload(from, to, options.merge(:via => :scp), &block)
+    end
+  end
+ 
+  Capistrano::Configuration.send(:include, UseScpForDeployment)
+
+  server "mercury.cyclonelabs.com", :web, :app, :db, primary: true
+
+  ssh_options[:port] = 23813
+
+  set :user, "babrovka"
+  set :application, "kzs"
+  set :deploy_to, "/srv/webdata/sakedev.kzsspb.ru"
+  set :use_sudo, false
+
+  set :scm, :none
+  set :repository, "."
+  set :deploy_via, :copy
+  set :local_repository, "."
+
+
+  set :branch, "master"
+
+  default_run_options[:pty] = true
+  ssh_options[:forward_agent] = true
+
 
   after "deploy", "deploy:cleanup"
 end
