6d473c23cc3130892ed9a2dba3f196628bcdfe47
justvitalius <justvitalius@gmail.com>
2014-02-10 18:36:23 +0400

    Delete old unit-tests.

--- test/factories/vehicle_users.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :vehicle_user do
-    vehicle_id "MyString"
-    user_id "MyString"
-  end
-end
