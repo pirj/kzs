680d37f5c140d88f80b2b788793d3167b08d2386
Sergey Prikhodko <prikha@gmail.com>
2014-02-10 13:10:57 +0400

    step into inherited_resources

--- app/controllers/documents/documents_controller.rb
+++ app/controllers/documents/documents_controller.rb
@@ -2,6 +2,9 @@ class Documents::DocumentsController < ResourceController
   layout 'base'
   actions :index
 
+  def index
+    @documents = Documents::ListDecorator.decorate(collection.includes(:sender_organization, :recipient_organization), with: Documents::ListShowDecorator)
+  end
 
 
 end
