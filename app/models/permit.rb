74e46a27b510cba1cc2cb1a2dc2b76cc4ae9fb9c
Dmitry T <atlancer@gmail.com>
2014-02-12 14:35:23 +0400

    changed validation in model Permit;

--- app/models/permit.rb
+++ app/models/permit.rb
@@ -1,4 +1,8 @@
 class Permit < ActiveRecord::Base
+  # TODO тесты
+  # TODO пропуск может быть удален ?
+  # TODO Если да: надо ли сохранять историю выдачи пропусков ? что делать со связями has_* (:dependent => :destroy) ?
+
   attr_accessible :number, :purpose, :start_date, :expiration_date, :requested_duration,
                   :granted_area, :granted_object, :permit_type, :agreed, :canceled,
                   :released, :issued, :permit_class, :vehicle_id, :date, :vehicle_attributes,
@@ -21,29 +25,32 @@ class Permit < ActiveRecord::Base
   scope :for_print, lambda { where("issued = ? AND rejected = ? AND canceled = ? AND expiration_date > ?", true, false, false, Date.today) }
   scope :applications, -> { where(agreed: false) }
 
-  attr_accessor :date, :drivers
+  attr_accessor :date, :drivers # TODO для чего эти методы ?
 
-  fields = %w[:number, :purpose, :start_date, :expiration_date, :requested_duration,
-                  :granted_area, :granted_object, :permit_type, :agreed, :canceled,
-                  :released, :issued, :permit_class, :vehicle_id, :date, :vehicle_attributes,
-                  :drivers, :user_attributes, :way_bill, :daily_pass_attributes, :vip_number, :user_id]
+  # TODO нужен normalize_attributes
 
-  fields.map{|f| validates f, :presence => true}
+  # TODO :agreed, :canceled, :released, :issued - нужна stat machine
+
+  # TODO расписать назначение полей
+  # TODO расписать валидацию для полей, не только presence
+  validates  :start_date, :expiration_date, :presence => true
+  #validates  :number, :purpose, :start_date, :expiration_date, :requested_duration,
+  #           :granted_area, :granted_object, :permit_type, :agreed, :canceled,
+  #           :released, :issued, :permit_class, :vehicle_id, :date,
+  #           :drivers, :way_bill, :vip_number, :user_id,
+  #           :vehicle_attributes, :user_attributes, :daily_pass_attributes,
+  #           :presence => true
 
   TYPES = %w[user vehicle daily]
   PERMIT_CLASSES = %w[standart vip]
-  
+
+  # TODO назначение метода ?
   def temporary
-    # TODO избыточная запись получается, можно просто:
-    # start_date == expiration_date
-    if self.start_date == self.expiration_date
-      true
-    else
-      false
-    end
+    self.start_date == self.expiration_date
   end
-  
-  def validity 
+
+  # TODO назначение метода ?
+  def validity
     if self.daily_pass.date
       '1'
     else
@@ -52,7 +59,8 @@ class Permit < ActiveRecord::Base
   end
 
   protected
-    def default_permit_class
-      self.permit_class = PERMIT_CLASSES.first
-    end
+
+  def default_permit_class
+    self.permit_class = PERMIT_CLASSES.first
+  end
 end
