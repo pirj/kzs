53122b5fea90f6f8ba32c22586eccbe317bf8d51
Tagir <tagirshine@yandex.ru>
2014-02-12 15:46:22 +0400

    dashboard changes

--- app/helpers/permits_helper.rb
+++ app/helpers/permits_helper.rb
@@ -44,7 +44,10 @@ module PermitsHelper
 
  #  content_tag(:span, "Hello world!", class: 'wasd')
 
+   #return permit.vehicle.register_sn
+   if permit.vehicle.register_sn
+     permit.vehicle.register_sn[0].to_s +  permit.vehicle.register_sn[1..3].to_s + permit.vehicle.register_sn[4..5].to_s + permit.vehicle.sn_region.to_s
 
-   return permit.vehicle.register_sn[0].to_s +  permit.vehicle.register_sn[1..3].to_s + permit.vehicle.register_sn[4..5].to_s + permit.vehicle.sn_region.to_s
+   end
   end
 end
