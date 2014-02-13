ec54493fc9ceb98ea1b0e543dc436e521d7f4aa3
justvitalius <justvitalius@gmail.com>
2014-02-13 21:46:36 +0400

    Add tooltips to form labels.

--- config/initializers/simple_form/simple_form.rb
+++ config/initializers/simple_form/simple_form.rb
@@ -62,6 +62,20 @@ SimpleForm.setup do |config|
     b.use :error, wrap_with: { tag: 'span', class: 'help-block has-error' }
   end
 
+  config.wrappers :inline_hint, :tag => 'div', :class => 'form-group', :error_class => 'error' do |b|
+    b.use :html5
+    b.use :placeholder
+
+    b.wrapper :label, tag: :label, class: 'control-label js-label-hint label-icon-hint' do |bl|
+      bl.use :label_text
+    end
+    b.wrapper :input_wrapper, tag: 'div' do |bi|
+      bi.use :input
+    end
+    b.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
+    b.use :error, wrap_with: { tag: 'span', class: 'help-block has-error' }
+  end
+
   config.wrappers :icon_prepend, :tag => 'div', :class => 'form-group', :error_class => 'error' do |b|
     b.use :html5
     b.use :placeholder
