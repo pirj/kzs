0cac9f6bd6f365a99444419a75acd8660f6fb0bf
Dmitry T <atlancer@gmail.com>
2014-02-13 15:01:27 +0400

    Added validation for DailyPass

--- app/models/daily_pass.rb
+++ app/models/daily_pass.rb
@@ -1,17 +1,58 @@
 class DailyPass < ActiveRecord::Base
-  attr_accessible :visitor_last_name,
-                  :permit_id,
-                  :last_name, :first_name, :middle_name,
-                  :id_type, :vehicle, :object, :person, :issued, :date, :guard_duty,
-                  :lp_r, :register_sn, :auto_mark, :auto_model,
-                  :first_letter, :second_letter, :third_letter, :sn_number, :id_series, :id_number,
-                  :has_vehicle
+  attr_accessible :permit_id,
+                  # Посетитель
+                  :last_name, :first_name, :middle_name, # Фамилия Имя Отчество
+                  # Документ посетителя
+                  :id_type,   # Вид документа
+                  :id_series, # серия документа
+                  :id_number, # номер документа
+
+                  :object,  # Куда следует
+                  :person,  # К кому
+                  :issued,  # Выдан (дата выдачи)
+
+                  # Транспортное средство (TC)
+                  :has_vehicle, # Посетитель на ТС ?
+                  :vehicle,     # Тип TC
+                  :auto_mark,   # Марка TC
+                  :auto_model,  # Модель TC
+                  # Государственный регистрационный номер ТС
+                  :first_letter,  # 1я буква номера
+                  :sn_number,     # Цифры номера
+                  :second_letter, # 2я буква номера
+                  :third_letter,  # 3я буква номера
+                  :lp_r,          # Номер региона
+
+                  :date,    # ?????????
+                  :guard_duty # Кто выдал пропуск (охранник\охранный пункт)
 
   attr_accessor :first_letter, :second_letter, :third_letter, :sn_number
 
   belongs_to :permit
 
   before_save :create_register_sn
+
+  # --------------------------------------------------------------------------------------------------------------------
+  # Validation
+
+  validates :permit_id, presence: true, on: :update
+
+  validates :last_name, :first_name, :middle_name,
+            :id_type, :id_series, :id_number,
+            :object, :person,
+            presence: true
+
+  validates :issued, presence: true # TODO need date validation (Date.parse, date >= Time.now, etc...)
+
+  validates :vehicle,
+            :auto_mark,
+            :auto_model,
+            :first_letter,
+            :sn_number,
+            :second_letter,
+            :third_letter,
+            :lp_r,
+            presence: { if: :has_vehicle }
 
   private
 
