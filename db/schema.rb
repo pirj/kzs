083a4acffbada386b4d1e3996c40815f9f4b40b4
Neodelf <neodelf@gmail.com>
2014-02-17 11:41:26 +0400

    Merge branch 'dev' of github.com:babrovka/kzs into dev
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- db/schema.rb
+++ db/schema.rb
@@ -120,6 +120,59 @@ ActiveRecord::Schema.define(:version => 20140214160046) do
     t.datetime "updated_at",  :null => false
   end
 
+  create_table "docs", :force => true do |t|
+    t.string   "title"
+    t.integer  "user_id"
+    t.integer  "organization_id"
+    t.text     "text"
+    t.string   "file_file_name"
+    t.string   "file_content_type"
+    t.integer  "file_file_size"
+    t.datetime "file_updated_at"
+    t.datetime "created_at",                                  :null => false
+    t.datetime "updated_at",                                  :null => false
+    t.integer  "recipient_id"
+    t.boolean  "sent",                     :default => false
+    t.integer  "approver_id"
+    t.boolean  "approved",                 :default => false
+    t.boolean  "opened",                   :default => false
+    t.boolean  "for_approve",              :default => false
+    t.boolean  "deleted",                  :default => false
+    t.boolean  "archived",                 :default => false
+    t.boolean  "callback",                 :default => false
+    t.boolean  "prepared",                 :default => false
+    t.boolean  "draft",                    :default => true
+    t.integer  "sender_organization_id"
+    t.string   "document_type"
+    t.boolean  "with_comments",            :default => false
+    t.boolean  "executed",                 :default => false
+    t.boolean  "for_confirmation",         :default => false
+    t.integer  "project_id"
+    t.boolean  "confidential",             :default => false
+    t.integer  "executor_id"
+    t.string   "attachment_file_name"
+    t.string   "attachment_content_type"
+    t.integer  "attachment_file_size"
+    t.datetime "attachment_updated_at"
+    t.string   "sn"
+    t.datetime "date"
+    t.integer  "version",                  :default => 1
+    t.integer  "document_conversation_id"
+    t.datetime "approved_date"
+    t.datetime "sent_date"
+    t.datetime "opened_date"
+    t.datetime "deteled_date"
+    t.datetime "prepared_date"
+    t.datetime "executed_date"
+    t.datetime "deadline"
+    t.integer  "ancestry"
+    t.boolean  "accepted",                 :default => false
+    t.boolean  "not_accepted",             :default => false
+    t.boolean  "with_completed_task_list", :default => false
+    t.datetime "accepted_date"
+    t.datetime "refuse_date"
+  end
+
   create_table "document_attachments", :force => true do |t|
     t.string   "attachment_file_name"
     t.string   "attachment_content_type"
@@ -329,6 +382,12 @@ ActiveRecord::Schema.define(:version => 20140214160046) do
     t.datetime "updated_at",  :null => false
   end
 
+  create_table "rights", :force => true do |t|
+    t.string   "title"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "statement_approvers", :force => true do |t|
     t.integer  "user_id"
     t.integer  "statement_id"
@@ -421,6 +480,13 @@ ActiveRecord::Schema.define(:version => 20140214160046) do
     t.integer  "permission_id"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
+  end
+
+  create_table "user_rights", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "right_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Organization do
 
   context 'mass assignment attributes' do
-    [:title, :short_title, :inn,:lft, :rgt, :phone, :official_mail,:tax_authority_that_registered, :parent_id, :director_id, :admin_id,
+    [:title, :short_title, :inn,:lft, :rgt, :phone, :mail,:tax_authority_that_registered, :parent_id, :director_id, :admin_id,
       :accountant_id, :bank_correspondent_account, :organization_account, :date_of_registration, :creation_resolution_date,
       :egrul_registration_date, :legal_address, :actual_address, :bank_address, :type_of_ownership, :certificate_of_tax_registration,
       :creation_resolution, :articles_of_organization, :logo, :egrul_excerpt, :kpp, :ogrn, :bik, :ogrn, :bik,:bank_bik, :bank_inn,
@@ -30,27 +30,32 @@ describe Organization do
   end
 
   context 'valid string type fields' do
+    it {should have_db_column(:title).of_type(:string)}
+  end
+
+
+  context 'valid string type fields' do
     [:title, :logo_file_name, :logo_content_type, :phone, :mail, :inn, :short_title, :type_of_ownership, :legal_address, :actual_address,
       :tax_authority_that_registered, :certificate_of_tax_registration_file_name, :certificate_of_tax_registration_content_type,
       :creation_resolution_file_name, :creation_resolution_content_type, :articles_of_organization_file_name,
       :articles_of_organization_content_type, :phone, :mail, :kpp, :ogrn, :bik, :egrul_excerpt_file_name, :egrul_excerpt_content_type,
       :bank_title, :bank_address, :bank_correspondent_account, :bank_bik, :bank_inn, :bank_kpp, :bank_okved, :organization_account]
-    .map{ |field| its(field) { should be_kind_of(String) } }
+    .map{ |field| it { have_db_column(field).of_type(:string)} }
   end
 
   context 'valid integer type fields' do
     [:parent_id, :lft, :rgt, :logo_file_size, :director_id, :admin_id, :certificate_of_tax_registration_file_size, :creation_resolution_file_size,
      :articles_of_organization_file_size, :accountant_id, :egrul_excerpt_file_size]
-    .map{ |field| it { should be_kind_of(Integer) } }
+    .map{ |field| it { have_db_column(field).of_type(:integer)} }
   end
   context 'valid datetime type fields' do
     [:created_at, :updated_at, :logo_updated_at, :date_of_registration, :certificate_of_tax_registration_updated_at, :creation_resolution_date,
      :creation_resolution_updated_at, :articles_of_organization_updated_at, :egrul_excerpt_updated_at]
-    .map{ |field| it { should be_kind_of(Datetime) } }
+    .map{ |field| it { have_db_column(field).of_type(:datetime)} }
   end
 
   context 'valid date type fields' do
     [:egrul_registration_date]
-    .map{ |field| it { should be_kind_of(Date) } }
+    .map{ |field| it { have_db_column(field).of_type(:date)} }
   end
 end
