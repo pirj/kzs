0beff8d1d81cef4fdc9f22f73b80d7e84839b45b
Sergey Prikhodko <prikha@gmail.com>
2014-02-25 23:07:13 +0400

    let class be configurable

--- app/helpers/documents/accountable_helper.rb
+++ app/helpers/documents/accountable_helper.rb
@@ -1,9 +1,11 @@
 module Documents::AccountableHelper
+  # TODO: @prikha it might have been put into form_helpers.
+  # Submit button can have content.
   def button_with_content(content, options)
     content_tag(:button, content, options )
   end
 
-  def submit_accountable(state)
-    button_with_content(t("helpers.submit.documents.to.#{state}"), name: "transition_to", value: state, class: "btn btn-primary")
+  def submit_accountable(state, html_class = 'btn-primary')
+    button_with_content(t("helpers.submit.documents.to.#{state}"), name: "transition_to", value: state, class: "btn #{html_class}")
   end
 end
