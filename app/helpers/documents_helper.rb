73f5e249065797f9d10688d44ff4024aa63d6c4d
Viacheslav Bobrov <babrovka@gmail.com>
2014-02-28 11:31:01 +0400

    document helper comment out

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -3,17 +3,15 @@
 module DocumentsHelper
   def pdf_to_png(document, width, height)
 
-    # TODO: in production after clear database this code "unless...end" must remove
-    path = "/srv/webdata/sakedev.kzsspb.ru/shared/system/documents/"
-    unless File.file?("/system/documents/document_#{document.id}.pdf") 
-      pdf = DocumentPdf.new(self, 'show')
-      pdf.render_file "#{path}document_#{id}.pdf"
-      pdf = Magick::Image.read("#{path}document_#{id}.pdf").first
-      thumb = pdf.scale(400, 520)
-      Dir.mkdir(path) unless File.exists?(path)
-      thumb.write "#{path}document_#{id}.png"
-    end
-    image_tag "#{path}document_#{document.id}.png", class: 'table-img-md'
+    #TODO: in production after clear database this code "unless...end" must remove
+    # unless File.file?("./tmp/document_#{document.id}.pdf")
+    #   pdf = DocumentPdf.new(document, 'show')
+    #   pdf.render_file "tmp/document_#{document.id}.pdf"
+    #   pdf = Magick::Image.read("tmp/document_#{document.id}.pdf").first
+    #   thumb = pdf.scale(400, 520)
+    #   thumb.write "public/system/documents/document_#{document.id}.png"
+    # end
+    image_tag "/system/documents/document_#{document.id}.png", class: 'table-img-md'
   end
 
   def attach_doc(doc)
