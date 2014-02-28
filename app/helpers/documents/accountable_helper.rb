ad4fdd320ffb0bc9bf993535f51eeeccf64592e5
justvitalius <justvitalius@gmail.com>
2014-02-28 17:56:50 +0400

    BugFix change-state-buttons translates in new document forms.

--- app/helpers/documents/accountable_helper.rb
+++ app/helpers/documents/accountable_helper.rb
@@ -6,7 +6,7 @@ module Documents::AccountableHelper
     submit_options = { name: 'transition_to', value: state, class: 'btn btn-default' }
     submit_options.merge!(options)
     submit_button(submit_options) do
-      t("helpers.submit.documents.to.#{state}")
+      t("activerecord.attributes.document.actions.#{state}")
     end
   end
 
