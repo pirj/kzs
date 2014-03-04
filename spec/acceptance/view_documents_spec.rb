b7cade8f5b9863f91e694657faa1cd21a273b75e
Tagir <tagirshine@yandex.ru>
2014-03-04 18:06:36 +0400

    test to documents view in process

--- spec/acceptance/view_documents_spec.rb
+++ spec/acceptance/view_documents_spec.rb
@@ -2,16 +2,20 @@ require 'acceptance/acceptance_helper'
 
 feature "List documents page correctly works", %q{} do
 
-  let(:path) { root_path }
+  let(:root) { root_path }
+  let(:doc_page_path) {documents_documents_path}
+  let!(:user) {FactoryGirl.create(:user)}
+  describe 'mail has a button to answer' do
 
-  describe 'not authenticate for unregisterable user' do
-    it 'should not authennticate with unregistarable user' do
-      visit path
 
-      sign_in_with 'user', 'user'
-
-      expect(page).to have_content('Авторизуйтесь, пожалуйста')
+    it 'view documents page opened' do
+      visit doc_page_path
+      sign_in_with user.email, "password"
+      #visit docPage
+      expect(page).to have_content('Документы')
     end
+
+
   end
 
 
