05dac7caed1531c1307d76377a2835188f125eb1
justvitalius <justvitalius@gmail.com>
2014-03-14 18:19:31 +0400

    BugFix highlight red border around date input.

--- app/inputs/datepicker_input.rb
+++ app/inputs/datepicker_input.rb
@@ -1,23 +1,32 @@
 class DatepickerInput < SimpleForm::Inputs::StringInput
   enable :value
+
+
   def input
-    out = '' # the output string we're going to build
-    _out = ''
-    input_html_classes.push 'form-control js-datepicker'
-
-    # because of original text_field-method override decorates date methods
-    input_html_options.merge!( value: @builder.object.send(attribute_name) )
-
-    _out << template.content_tag(:div, class: 'input-group-icon') do
-              template.content_tag(:span, nil, class: 'fa fa-calendar')
-    end
-
-    _out << (@builder.text_field(attribute_name, input_html_options))
-
-    out << template.content_tag(:div, class: 'input-group') do
-              _out.html_safe
+    template.content_tag(:div, class: 'input-group') do
+      icon + text_field
     end.html_safe
-    # append input as string.
-    _out.html_safe
   end
+
+  protected
+
+  def text_field
+    @builder.text_field(attribute_name, input_html_options)
+  end
+
+  def input_html_classes
+    super.push 'form-control js-datepicker'
+  end
+
+  def input_html_options
+    # because of original text_field-method override decorates date methods
+    super.merge!( value: @builder.object.send(attribute_name) )
+  end
+
+  def icon
+    template.content_tag(:div, class: 'input-group-icon') do
+      template.content_tag(:span, nil, class: 'fa fa-calendar')
+    end
+  end
+
 end
