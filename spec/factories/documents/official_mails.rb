d826e1a2320d7166be1cf9d5d81796e9f702f70d
Vladimir I. Krivchenko <vladimir.krivchenko@gmail.com>
2014-03-24 20:13:59 +0400

    Official Mails test factory: common code moved to top, added new mail types

--- spec/factories/documents/official_mails.rb
+++ spec/factories/documents/official_mails.rb
@@ -1,27 +1,10 @@
 FactoryGirl.define do
   factory :mail, class: Documents::OfficialMail do
+
+    # Общее
     title 'title'
     body 'body'
     sender_organization
-
-    factory :mail_with_direct_recipient do
-      recipient_organization
-
-      factory :approved_mail do
-        after(:create) do |instance, ev|
-          instance.transition_to! :prepared
-          instance.transition_to! :approved
-        end
-      end
-    end
-
-    factory :mail_with_many_recipients do
-      recipients {
-        5.times.map do
-          FactoryGirl.create(:recipient_organization)
-        end
-      }
-    end
 
     after(:build) do |instance, ev|
       user = instance.sender_organization.admin
@@ -33,5 +16,50 @@ FactoryGirl.define do
     after(:create) do |instance,ev|
       instance.transition_to!('draft')
     end
+
+    # Частные случаи
+
+    # С адресатом
+    factory :mail_with_direct_recipient do
+      recipient_organization
+
+      factory :approved_mail do
+        after(:create) do |instance, ev|
+          instance.transition_to! :prepared
+          instance.transition_to! :approved
+        end
+      end
+    end
+
+    # С несколькими адресатами
+    factory :mail_with_many_recipients do
+      recipients {
+        5.times.map do
+          FactoryGirl.create(:recipient_organization)
+        end
+      }
+    end
+
+    # Подписанная, с адресатом
+    factory :mail_approved do
+      recipient_organization
+
+      after(:create) do |instance,ev|
+        instance.transition_to!('prepared')
+        instance.transition_to!('approved')
+      end
+    end
+
+    # С приаттаченным mail_approved
+    factory :mail_with_attachment do
+      recipient_organization
+
+      after(:create) do |instance,ev|
+        attachment = FactoryGirl.create(:mail_approved)
+        instance.attached_documents << attachment.document
+      end
+    end
+
+    
   end
 end
