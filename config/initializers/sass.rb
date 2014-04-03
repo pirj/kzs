ae283b28763b803dc7fecde23e3ac176cde60111
justvitalius <justvitalius@gmail.com>
2014-04-03 11:20:44 +0400

    Add tooltips to sortable table caption.

--- config/initializers/sass.rb
+++ config/initializers/sass.rb
@@ -1,4 +1,7 @@
 require 'sprockets/railtie'
+#Rails.application.assets.register_postprocessor 'text/css', :sass_bem_class_builder do |context, data|
+#  data.gsub(/([a-zA-Z0-9\-]+)[ ]([_]+)/i){ $1 + $2 }
+#end
 Rails.application.assets.register_postprocessor 'text/css', :sass_bem_class_builder do |context, data|
   data.gsub(/([a-zA-Z0-9\-]+)[ ]([_]+)/i){ $1 + $2 }
 end
