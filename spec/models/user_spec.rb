6e50d24a91ab1db3c970d1fbc7961f0d954c9d3f
Vladimir I. Krivchenko <vladimir.krivchenko@gmail.com>
2014-04-07 18:30:36 +0400

    Add failing specs for user for conforming and denying a document

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -89,4 +89,58 @@ describe User do
       it { should be_false }
     end
   end
+
+  context 'conforming documents' do
+    before :each do
+      @mail_to_conform = FactoryGirl.create(:mail_with_direct_recipient_and_conformers)
+      @conformer = mail_to_conform.conformers.first
+    end
+
+    context "When in the conformers list" do
+      it "should be able to conform document" do
+        expect {@conformer.conform(@mail_to_conform)}.not_to raise_error
+        @mail_to_conform.conformed_users.should include @conformer
+      end  
+
+      it "should be able to optionnaly add a comment when conforming" do
+        @conformer.conform @mail_to_conform, comment: "Comment!"
+
+        expect {@conformer.conform @mail_to_conform}.not_to raise_error
+      end
+
+      it "should be able to deny document" do
+        expect {@conformer.conform(@mail_to_conform)}.not_to raise_error
+        @mail_to_conform.conformed_users.should include @conformer
+      end
+
+      it "shouldn't be able to deny the document without a comment or with a nil/empty/space-only-characters comment" do
+        expect {@conformer.deny @mail_to_conform}.to raise_error # Number of arguments 
+        expect {@conformer.deny @mail_to_conform, nil}.to raise_error # Number of arguments 
+        expect {@conformer.deny @mail_to_conform, ''}.to raise_error # Empty comment
+        expect {@conformer.deny @mail_to_conform, '   '}.to raise_error # Considering comment empty
+      end
+
+      it "shouldn't be able to conform/deny a document if he already made a decision" do
+        @conformer.conform @mail_to_conform
+        expect { @conformer.conform @mail_to_conform }.to raise_error # Повторная попытка согласования
+        expect { @conformer.deny @mail_to_conform, 'Comment!' }.to raise_error # Попытка изменить свое мнение
+      end
+    end
+
+    context "When not in the conformers list" do
+      it "shouldn't be able to conform document, if it's not in the conformers list" do
+        user = FactoryGirl.create(:user)
+
+        expect {user.conform(@mail_to_conform)}.to raise_error
+        @mail_to_conform.conformed_users.should_not include user
+      end
+
+      it "shouldn't be able to deny document (with non-empty comment), if it's not in the conformers list" do
+        user = FactoryGirl.create(:user)
+
+        expect {user.conform(@mail_to_conform)}.to raise_error
+        @mail_to_conform.conformed_users.should_not include user
+      end
+    end
+  end
 end
