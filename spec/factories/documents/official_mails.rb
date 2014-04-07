14b63887326ade2ea78619bc2457e7dfa74c8fc7
Vladimir I. Krivchenko <vladimir.krivchenko@gmail.com>
2014-04-07 15:04:02 +0400

    Cleanup mails factories, added prepared mail factory

--- spec/factories/documents/official_mails.rb
+++ spec/factories/documents/official_mails.rb
@@ -1,6 +1,6 @@
 # Добавляет к Accountable приаттаченный документ mail_approved
 def add_attachment_to(accountable)
-  attachment = FactoryGirl.create(:mail_approved)
+  attachment = FactoryGirl.create(:approved_mail)
   accountable.attached_documents << attachment.document
 end
 
@@ -29,10 +29,17 @@ FactoryGirl.define do
     factory :mail_with_direct_recipient do
       recipient_organization
 
-      factory :approved_mail do
+      # Подготовленное  письмо
+      factory :prepared_mail do
         after(:create) do |instance, ev|
           instance.transition_to! :prepared
-          instance.transition_to! :approved
+        end
+        
+        # Подписанное письмо
+        factory :approved_mail do
+          after(:create) do |instance, ev|
+            instance.transition_to! :approved
+          end
         end
       end
 
@@ -51,22 +58,6 @@ FactoryGirl.define do
           FactoryGirl.create(:recipient_organization)
         end
       }
-
-      factory :mail_with_attached_documents do
-        after(:create) do |instance, ev|
-          instance.attached_documents << FactoryGirl.create(:mail_with_direct_recipient).document << FactoryGirl.create(:mail_with_direct_recipient).document
-        end
-      end
-    end
-
-    # Подписанная, с адресатом
-    factory :mail_approved do
-      recipient_organization
-
-      after(:create) do |instance,ev|
-        instance.transition_to!('prepared')
-        instance.transition_to!('approved')
-      end
     end
 
     # С приаттаченным 1 или более mail_approved
