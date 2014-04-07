274169af51d230eaf7871e0eac4876b33f650740
justvitalius <justvitalius@gmail.com>
2014-04-07 13:50:29 +0400

    Fix disabling input-with-checkbox with selected options.

--- app/inputs/select_with_checkbox_input.rb
+++ app/inputs/select_with_checkbox_input.rb
@@ -1,4 +1,14 @@
 # coding: utf-8
+# рисуем контрол состоящий из чекбокса и инпута.
+# чекбокс имеет два варианта отрисовки:
+# — с лэйблом (справа от чекбокса)
+# — без лейбла
+#
+# если значения в инпуте были выбраны и сохранены в БД, то ставим галку в чекбоксе и делаем активным инпут
+# если значений в инпуте нет, то галку убираем и дизэйблим инпут
+#
+# ВНИМАНИЕ!
+# для полноценной работы на фронтенде требуется js-скрипты
 class SelectWithCheckboxInput < SimpleForm::Inputs::CollectionInput
 
   def input
@@ -12,10 +22,16 @@ class SelectWithCheckboxInput < SimpleForm::Inputs::CollectionInput
   protected
 
   def updated_html_options
-    required_options = { data: { target: uniq_name }, disabled: :disabled }
+    required_options = { data: { target: uniq_name } }
+    required_options[:disabled] = :disabled if input_options_blank? # если в селекте не выбраны значения, то дизэйблим его
     input_html_classes.push('js-chosen') # добавляем, чтобы включить chosen
     input_html_options[:data].merge!(required_options[:data]) if input_html_options[:data].present? # иначе никак не слить data атрибуты,чтобы не потерять их из разных мест
     required_options.merge(input_html_options) # окончательно сливаем элементы первого уровня
+  end
+
+  # определяем, есть ли сохраненные значения в данном селекте
+  def input_options_blank?
+    @builder.object.try(attribute_name).blank?
   end
 
   def select
@@ -29,7 +45,8 @@ class SelectWithCheckboxInput < SimpleForm::Inputs::CollectionInput
   end
 
   def checkbox
-    template.check_box_tag("is_#{attribute_name}".to_sym, nil, false, class: 'js-active-input', data: { target: uniq_name  })
+    checked = input_options_blank? ? false : true # если значения в селекте есть, то ставим галочку
+    template.check_box_tag("is_#{attribute_name}".to_sym, nil, checked, class: 'js-active-input', data: { target: uniq_name  })
   end
 
   def checkbox_title(&block)
@@ -39,6 +56,9 @@ class SelectWithCheckboxInput < SimpleForm::Inputs::CollectionInput
     end
   end
 
+  # рисуем чекбокс
+  # — с лэйблом
+  # — без лейбла
   def checkbox_with_title
     template.content_tag(:div, class: "col-sm-#{check_box_col}") do
       template.content_tag(:div, class: 'checkbox') do
@@ -57,10 +77,12 @@ class SelectWithCheckboxInput < SimpleForm::Inputs::CollectionInput
     { class: 'js-label-hint label-icon-hint', title: checkbox_hint} if has_checkbox_hint?
   end
 
+  # длина колонок бутстрапа для чекбокса
   def check_box_col
     has_checkbox_text? ? 4 : 1
   end
 
+  # уникальное имя для чекбокса, по нему происходит связь между чекбоксом и инпутом на фронтенде через js-скрипт
   def uniq_name
     @select_with_checkbox_input_uniq_time ||= "#{attribute_name}-#{(DateTime.now.to_f*10**5).to_i.to_s}"
   end
