2f4e865a70896abdb76a1c04c98a6e3b97a323fe
Tagir <tagirshine@yandex.ru>
2014-04-14 16:05:08 +0400

    add test fot conformer

--- spec/acceptance/documents/conformers/official_mail_conformer_spec.rb
+++ spec/acceptance/documents/conformers/official_mail_conformer_spec.rb
@@ -1,48 +1,84 @@
 require 'acceptance/acceptance_helper'
 
 feature "Users view agreement-conformer buttons", %q() do
-  let(:document) { FactoryGirl.create(:order) }
-  let(:user) { FactoryGirl.create(:user) }
-  let(:recipient_user) { document.recipient_organization.admin }
-  let(:sender_user) { document.sender_organization.admin }
-  let(:path) {  documents_order_path(document) }
+  let(:accountable) { FactoryGirl.create(:mail_with_direct_recipient_and_conformers) }
+  let(:approver) { accountable.approver }
+  # let(:recipient_user) { accountable.recipient_organization.admin }
+  # let(:sender_user) { accountable.sender_organization.admin }
+  let(:path) {  polymorphic_path(accountable) }
 
-  background do
-    visit path
-    sign_in_with sender_user.email, 'password'
+
+  #subject { page }
+
+
+
+  describe 'user can not use someone else`s agreement ', js: true do
+
+    background do
+      visit path
+      sign_in_with accountable.conformers.first.email, 'password'
+    end
+
+
+
+    context 'creator' do
+      let(:user) { accountable.creator }
+
+
+
+      it 'should use own agreement-button' do
+        # проверяем что кнопка юзера нажимается
+
+          expect(page).to have_selector('.js-document-conform-agree-btn')
+          expect(all('.js-document-conform-agree-btn').count).to eq(1)
+
+      end
+    end
+
+
   end
 
+  
 
-  describe 'user can not use someone else`s agreement ' do #4
-
-    it 'should use own agreement-button' do
-      # проверяем что кнопка юзера нажимается
-    end
-    it 'shouldn`t use foreign agreement-button' do
-      # проверяем что чужие кнопки заблокированы
-    end
-
-  end
-
-  describe 'user can not vote again ' do  #1
-
-    it 'no button after vote' do
-      #голосуем, проверяем что кнопку больше не нажать
-    end
-
-  end
-
-  describe 'user must create comment if agreement negative' do  #2
-    it 'negative vote without comment should not be created' do
-      #пытаеся сделать подтверждение с пустым окном комментария
-
-    end
-
-    it 'negative vote with comment should be created' do
-      #должно пройти
-    end
-
-  end
+  #describe 'user can not vote again ' , js: true do
+  #  background do
+  #    visit path
+  #    sign_in_with accountable.conformers.first.email, 'password'
+  #  end
+  #
+  #  context 'creator' do
+  #
+  #
+  #    let(:user) { accountable.creator }
+  #
+  #
+  #    it 'no button after vote' do
+  #      #голосуем, проверяем что кнопку больше не нажать
+  #      skip_welcome
+  #      within('.conformers-list') do
+  #        #first('.js-document-conform-deny-btn').click
+  #        page.execute_script("$('.js-document-conform-agree-btn')[0].click()")
+  #
+  #      end
+  #      create_screenshot
+  #      expect(all('.js-document-conform-agree-btn').count).to eq(4)
+  #
+  #      #page.fill_in 'Name', :with => 'Bob'
+  #    end
+  #  end
+  #end
+  #
+  #describe 'user must create comment if agreement negative' do  #2
+  #  it 'negative vote without comment should not be created' do
+  #    #пытаеся сделать подтверждение с пустым окном комментария
+  #
+  #  end
+  #
+  #  it 'negative vote with comment should be created' do
+  #    #должно пройти
+  #  end
+  #
+  #end
 
 
 end
