052208bc55e9e7d7213db9bc78029d87e4c0023a
Tagir <tagirshine@yandex.ru>
2014-04-14 18:01:41 +0400

    new test for conformer

--- spec/acceptance/documents/conformers/official_mail_conformer_spec.rb
+++ spec/acceptance/documents/conformers/official_mail_conformer_spec.rb
@@ -40,34 +40,45 @@ feature "Users view agreement-conformer buttons", %q() do
 
   
 
-  #describe 'user can not vote again ' , js: true do
-  #  background do
-  #    visit path
-  #    sign_in_with accountable.conformers.first.email, 'password'
-  #  end
-  #
-  #  context 'creator' do
-  #
-  #
-  #    let(:user) { accountable.creator }
-  #
-  #
-  #    it 'no button after vote' do
-  #      #голосуем, проверяем что кнопку больше не нажать
-  #      skip_welcome
-  #      within('.conformers-list') do
-  #        #first('.js-document-conform-deny-btn').click
-  #        page.execute_script("$('.js-document-conform-agree-btn')[0].click()")
-  #
-  #      end
-  #      create_screenshot
-  #      expect(all('.js-document-conform-agree-btn').count).to eq(4)
-  #
-  #      #page.fill_in 'Name', :with => 'Bob'
-  #    end
-  #  end
-  #end
-  #
+  describe 'user can not vote again ' , js: true do
+    background do
+      visit path
+      sign_in_with accountable.conformers.first.email, 'password'
+    end
+
+    context 'creator' do
+
+
+      let(:user) { accountable.creator }
+
+
+      it 'no button after vote' do
+        #голосуем, проверяем что кнопку больше не нажать
+        skip_welcome
+        within('.conformers-list') do
+
+          page.execute_script("$('.js-document-conform-agree-btn')[0].click()")
+
+        end
+        sleep(1)
+
+        fill_in 'conformation_comment', :with => "10"
+
+        first('.spec-submit-comment').click
+        sleep(1)
+
+        within('.conformers-list') do
+
+          page.execute_script("$('.js-document-conform-agree-btn')[0].click()")
+
+        end
+
+        #expect(all('.js-document-conform-txt:visible').count).to eq(0)
+        expect(page).to have_selector('.js-document-conform-txt', visible: false)
+      end
+    end
+  end
+
   #describe 'user must create comment if agreement negative' do  #2
   #  it 'negative vote without comment should not be created' do
   #    #пытаеся сделать подтверждение с пустым окном комментария
