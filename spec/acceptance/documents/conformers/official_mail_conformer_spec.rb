601568d9cf963838ca9a2ea17d3292663ad4ee61
Tagir <tagirshine@yandex.ru>
2014-04-15 12:42:48 +0400

    updated confirm-test

--- spec/acceptance/documents/conformers/official_mail_conformer_spec.rb
+++ spec/acceptance/documents/conformers/official_mail_conformer_spec.rb
@@ -3,14 +3,7 @@ require 'acceptance/acceptance_helper'
 feature "Users view agreement-conformer buttons", %q() do
   let(:accountable) { FactoryGirl.create(:mail_with_direct_recipient_and_conformers) }
   let(:approver) { accountable.approver }
-  # let(:recipient_user) { accountable.recipient_organization.admin }
-  # let(:sender_user) { accountable.sender_organization.admin }
   let(:path) {  polymorphic_path(accountable) }
-
-
-  #subject { page }
-
-
 
   describe 'user can not use someone else`s agreement ', js: true do
 
@@ -18,24 +11,12 @@ feature "Users view agreement-conformer buttons", %q() do
       visit path
       sign_in_with accountable.conformers.first.email, 'password'
     end
-
-
-
-    context 'creator' do
-      let(:user) { accountable.creator }
-
-
-
       it 'should use own agreement-button' do
         # проверяем что кнопка юзера нажимается
 
           expect(page).to have_selector('.js-document-conform-agree-btn')
           expect(all('.js-document-conform-agree-btn').count).to eq(1)
-
       end
-    end
-
-
   end
 
   
@@ -46,65 +27,60 @@ feature "Users view agreement-conformer buttons", %q() do
       sign_in_with accountable.conformers.first.email, 'password'
     end
 
-    context 'creator' do
-
-
-      let(:user) { accountable.creator }
-
-
-      it 'no button after vote' do
+      scenario 'no button after vote' do
         #голосуем, проверяем что кнопку больше не нажать
         skip_welcome
         within('.conformers-list') do
-
           page.execute_script("$('.js-document-conform-agree-btn')[0].click()")
-
         end
         sleep(1)
-
         fill_in 'conformation_comment', :with => "10"
-
         first('.spec-submit-comment').click
         sleep(1)
-
-        #within('.conformers-list') do
-        #
-        #  page.execute_script("$('.js-document-conform-agree-btn')[0].click()")
-        #
-        #end
-
-        #expect(all('.js-document-conform-txt:visible').count).to eq(0)
         expect(page).to have_selector('.js-document-conform-txt', visible: false)
       end
-    end
+
   end
 
-  describe 'user must create comment for negative voting', js:true do
-    background do
-      visit path
-      sign_in_with accountable.conformers.first.email, 'password'
-    end
-
-    it 'vote without comment should not be validate' do
-      skip_welcome
-      within('.conformers-list') do
-
-        page.execute_script("$('.js-document-conform-deny-btn')[0].click()")       #сдвинули ползунок
-
+  context 'negative vote' do
+    describe 'user must create required comment', js:true do
+      background do
+        visit path
+        sign_in_with accountable.conformers.first.email, 'password'
+        skip_welcome
+        within('.conformers-list') do
+          page.execute_script("$('.js-document-conform-deny-btn')[0].click()")       #сдвинули ползунок
+        end
+        sleep(1)
       end
-      sleep(1)
-      fill_in 'conformation_comment', :with => 'exampletext'
-      sleep(1)
-      fill_in 'conformation_comment', :with => ''
-      sleep(1)
-      first('.spec-submit-comment').click                               #жмем кнопку отправить комментарий
+
+      scenario 'disabling to send empty comment' do
+        expect(page).to have_xpath("//input[contains(@class,'spec-submit-comment') and @disabled]")
+      end
 
 
-      create_screenshot
-      expect(page).to have_selector('.js-document-conform-txt', visible: true)
+      scenario 'disabling to send fill-and-empty comment' do
+        fill_in 'conformation_comment', :with => 'exampletext'
+        sleep(1)
+        expect(page).to_not have_xpath("//input[contains(@class,'spec-submit-comment') and @disabled]")
+        fill_in 'conformation_comment', :with => ''
+        sleep(1)
+        first('.spec-submit-comment').click                               #жмем кнопку отправить комментарий
+        expect(page).to have_selector('.js-document-conform-txt', visible: true)
+      end
 
+      scenario 'displaying sent comment' do
+        expect(page).to_not have_content 'exampletext'
+
+        fill_in 'conformation_comment', :with => 'exampletext'
+        sleep(1)
+        first('.spec-submit-comment').click                               #жмем кнопку отправить комментарий
+
+        expect(page).to have_selector('.js-document-conform-txt', visible: false)
+        expect(page).to have_content 'Комментарии'
+        expect(page).to have_content 'exampletext'
+      end
     end
-
   end
 
 
