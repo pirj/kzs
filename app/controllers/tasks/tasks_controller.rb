fbd6d78d6353716075223e9e429a5cefbf96f0ca
Tagir <tagirshine@yandex.ru>
2014-04-21 16:31:28 +0400

    added organizations - task

--- app/controllers/tasks/tasks_controller.rb
+++ app/controllers/tasks/tasks_controller.rb
@@ -2,6 +2,17 @@ class Tasks::TasksController < InheritedResources::Base
   layout 'base'
 
   def index
-    @tasks = Tasks::Task.order('created_at DESC')
+    @tasks = Tasks::Task.for_org(current_organization)
   end
+
+  def create
+    @task = Tasks::Task.create(params[:tasks_task])
+    @task.organization_id = current_user.organization_id
+    if @task.save
+      redirect_to @task, :notice => 'Задача успешно поставлена'
+    else
+      render :new
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140418085216) do
+ActiveRecord::Schema.define(:version => 20140421115717) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -438,8 +438,9 @@ ActiveRecord::Schema.define(:version => 20140418085216) do
     t.text     "text"
     t.datetime "started_at"
     t.datetime "finished_at"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.integer  "organization_id"
   end
 
   create_table "tasks_tasks_approvers", :id => false, :force => true do |t|
