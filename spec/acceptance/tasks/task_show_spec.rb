e49514e34a9d5da0ac991aa9799aea4bfad1fec1
levtolstoi <light.n.night@gmail.com>
2014-04-22 17:05:22 +0400

    Add task show spec complete

--- spec/acceptance/tasks/task_show_spec.rb
+++ spec/acceptance/tasks/task_show_spec.rb
@@ -11,7 +11,7 @@ feature "Users see show-page task", %q() do
   let!(:user_2) { FactoryGirl.create(:user, organization: user.organization) }
 
   let(:task) { FactoryGirl.create(:task) }
-  let(:show_path) {  tasks_path(task) }
+  let(:show_path) {  task_path(task) }
 
   background do
     visit root_path
@@ -21,16 +21,16 @@ feature "Users see show-page task", %q() do
 
   describe 'create new task and show', js: true do
     background do
-      visit tasks_path(task)
+      visit task_path(task)
     end
 
     context 'show fill args' do
       scenario 'show args' do
-        fill_in 'Контрольные лица'
-        fill_in 'Исполнители'
-        fill_in 'Дата начала'
-        fill_in 'Дата окончания '
-        fill_in 'Описание '
+        expect(page).to have_content 'Контрольные лица'
+        expect(page).to have_content 'Исполнители'
+        expect(page).to have_content 'Дата начала'
+        expect(page).to have_content 'Дата окончания '
+        expect(page).to have_content 'Описание '
       end
     end
   end
