81e6fb47a26a1cfe13be59117654b76ca11ee53e
Tagir <tagirshine@yandex.ru>
2014-04-22 15:21:44 +0400

    diagram view tasks

--- config/routes.rb
+++ config/routes.rb
@@ -160,7 +160,7 @@ Kzs::Application.routes.draw do
     namespace :api, defaults: { format: 'json' } do
       resources :tasks
     end
-    get 'tasks/gantt' => 'task/tasks#gantt'
+    get 'tasks/gantt' => 'tasks#gantt'
     resources :tasks
   end
 
