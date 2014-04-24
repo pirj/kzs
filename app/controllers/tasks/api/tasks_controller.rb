93ceccf8aff405cf93e87d676162eefa0cf4e2aa
justvitalius <justvitalius@gmail.com>
2014-04-24 16:47:23 +0400

    Fix errors with update tasks from Gantt and refactor permitting params in tasks api.

--- app/controllers/tasks/api/tasks_controller.rb
+++ app/controllers/tasks/api/tasks_controller.rb
@@ -1,8 +1,8 @@
 class Tasks::Api::TasksController < ResourceController
   layout nil
 
+  before_filter :permit_params, only: [:create, :update]
   has_scope :for_organization, only: [:index]
-
   respond_to :json
 
   def index
@@ -12,7 +12,7 @@ class Tasks::Api::TasksController < ResourceController
   end
 
   def create
-    @task = Tasks::Task.new(mapped_post_params).tap do |task|
+    @task = Tasks::Task.new(params[:tasks_task]).tap do |task|
       task.organization = current_user.organization
       # заглушка на первое время,чтобы не выключать валидации в модели
       task.approvers << User.where(organization_id: current_user.organization.id).first
@@ -27,7 +27,6 @@ class Tasks::Api::TasksController < ResourceController
     @task.organization ||= current_user.organization
     @task.approvers << User.where(organization_id: current_user.organization.id).first  if @task.approvers.blank?
     @task.executors << User.where(organization_id: current_user.organization.id).last   if @task.executors.blank?
-    params = mapped_post_params
     super
   end
 
@@ -52,26 +51,25 @@ class Tasks::Api::TasksController < ResourceController
     (task.finished_at - task.started_at).to_i/1.day
   end
 
+
+  # преобразуем пришедшие параметры
+  # пропускаем только то, что можно пропустить
+  def permit_params
+    permitted = [:end_date, :start_date, :title, :description]
+    params[:tasks_task].reject! do |k,v|
+      !permitted.include?(k.to_sym)
+    end
+    mapping_post_params
+  end
+
   # преобразуем параметры пришедшие из клиентского js в наши собственные
   # по причине использования чужой библиотеки, не можем изменить клиентские параметры задачи
-  def mapped_post_params
-    p = permitted_post_params
+  def mapping_post_params
+    p = params[:tasks_task]
     p[:text] = p.delete(:description.to_s)
     p[:finished_at] = p.delete(:end_date.to_s)
     p[:started_at] = p.delete(:start_date.to_s)
     p
-  end
-
-
-  def post_params
-    params[:tasks_task]
-  end
-
-
-  # выбираем только разрешенные атрибуты из пришедших параметров
-  def permitted_post_params
-    permitted = [:end_date, :start_date, :title, :description]
-    post_params.select { |k,v| permitted.include?(k.to_sym) }
   end
 
 end
