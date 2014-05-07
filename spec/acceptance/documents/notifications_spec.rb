e824073d5c3f4558d455e3f2ea12d88f21a2b6ce
justvitalius <justvitalius@gmail.com>
2014-05-07 13:48:32 +0400

    Restore all notification specs.

--- spec/acceptance/documents/notifications_spec.rb
+++ spec/acceptance/documents/notifications_spec.rb
@@ -14,90 +14,90 @@ feature "User receives documents notifications", %q() do
   let(:executor)  { mail.executor }
   let(:conformer) { mail.conformers.first }
 
-  # describe 'notifications without changes in documents' do
-  #   context 'without notifiable documents' do
-  #     let(:user){ mail.sender_organization.director }
-  #
-  #     background do
-  #       visit unreadable_path
-  #       sign_in_with user.email
-  #       visit unreadable_path
-  #       expect(current_url).to eq documents_url(with_type: 'incoming')
-  #     end
-  #
-  #
-  #     context 'without changing and notifications' do
-  #       [:executor, :creator, :approver, :conformer].each do |_user|
-  #         context _user do
-  #           let(:user) { send(_user) }
-  #
-  #           scenario 'empty notifications badges' do
-  #             expect(page).to_not have_css '.spec-notification-badge'
-  #           end
-  #
-  #           scenario 'empty document list' do
-  #             expect(page).to have_content 'по заданным параметрам фильтрации документов нет'
-  #           end
-  #
-  #         end
-  #       end
-  #     end
-  #
-  #   end
-  # end
+  describe 'notifications without changes in documents' do
+    context 'without notifiable documents' do
+      let(:user){ mail.sender_organization.director }
 
-  # describe 'notifications after saving document' do
-  #   context 'saving document without changes' do
-  #     # оповещения создаются по-настоящему в контроллерах, никаких колбеков в моделях
-  #     # поэтому нужно зайти под главным и протолкнуть письмо далее по цепочке статусов
-  #     background do
-  #       sign_out
-  #       visit edit_path
-  #       sign_in_with approver.email
-  #       expect(current_path).to eq edit_path
-  #       click_on 'Сохранить'
-  #
-  #       sign_out
-  #       sign_in_with user.email
-  #       visit unreadable_path
-  #     end
-  #
-  #     [:executor, :conformer].each do |_user|
-  #       context _user do
-  #         let(:user) { send(_user) }
-  #
-  #         scenario 'not empty document list' do
-  #           expect(page).to_not have_content 'по заданным параметрам фильтрации документов нет'
-  #           expect(page).to have_content mail.title
-  #         end
-  #
-  #         scenario 'badge about one notification documents count' do
-  #           page.all('.spec-notification-badge').each do |elem|
-  #             expect(elem).to have_content '1'
-  #           end
-  #         end
-  #
-  #       end
-  #     end
-  #
-  #     # у aprrover другое условие, потому что он является последним,
-  #     # кто внес редакции в документ, он «как-бы в курсе» изменений документа
-  #     # поэтому у него и отдельный сценарий
-  #     context 'approver' do
-  #       let(:user) { approver }
-  #
-  #       scenario 'not empty document list' do
-  #         expect(page).to_not have_content 'по заданным параметрам фильтрации документов нет'
-  #         expect(page).to have_content mail.title
-  #       end
-  #
-  #       scenario 'empty notification badge' do
-  #         expect(page).to_not have_css '.spec-notification-badge'
-  #       end
-  #
-  #     end
-  #   end
-  # end
+      background do
+        visit unreadable_path
+        sign_in_with user.email
+        visit unreadable_path
+        expect(current_url).to eq documents_url(with_type: 'incoming')
+      end
+
+
+      context 'without changing and notifications' do
+        [:executor, :creator, :approver, :conformer].each do |_user|
+          context _user do
+            let(:user) { send(_user) }
+
+            scenario 'empty notifications badges' do
+              expect(page).to_not have_css '.spec-notification-badge'
+            end
+
+            scenario 'empty document list' do
+              expect(page).to have_content 'по заданным параметрам фильтрации документов нет'
+            end
+
+          end
+        end
+      end
+
+    end
+  end
+
+  describe 'notifications after saving document' do
+    context 'saving document without changes' do
+      # оповещения создаются по-настоящему в контроллерах, никаких колбеков в моделях
+      # поэтому нужно зайти под главным и протолкнуть письмо далее по цепочке статусов
+      background do
+        sign_out
+        visit edit_path
+        sign_in_with approver.email
+        expect(current_path).to eq edit_path
+        click_on 'Сохранить'
+
+        sign_out
+        sign_in_with user.email
+        visit unreadable_path
+      end
+
+      [:executor, :conformer].each do |_user|
+        context _user do
+          let(:user) { send(_user) }
+
+          scenario 'not empty document list' do
+            expect(page).to_not have_content 'по заданным параметрам фильтрации документов нет'
+            expect(page).to have_content mail.title
+          end
+
+          scenario 'badge about one notification documents count' do
+            page.all('.spec-notification-badge').each do |elem|
+              expect(elem).to have_content '1'
+            end
+          end
+
+        end
+      end
+
+      # у aprrover другое условие, потому что он является последним,
+      # кто внес редакции в документ, он «как-бы в курсе» изменений документа
+      # поэтому у него и отдельный сценарий
+      context 'approver' do
+        let(:user) { approver }
+
+        scenario 'not empty document list' do
+          expect(page).to_not have_content 'по заданным параметрам фильтрации документов нет'
+          expect(page).to have_content mail.title
+        end
+
+        scenario 'empty notification badge' do
+          expect(page).to_not have_css '.spec-notification-badge'
+        end
+
+      end
+    end
+  end
 
   describe 'notifications after editing document', js: true do
     context 'old interested people' do
