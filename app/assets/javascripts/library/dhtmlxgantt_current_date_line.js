1890a8367c135a14e7038da27b70a97892b3fb87
levtolstoi <light.n.night@gmail.com>
2014-05-19 18:51:46 +0400

    Fix current date for gantt_time_line

--- app/assets/javascripts/library/dhtmlxgantt_current_date_line.js
+++ app/assets/javascripts/library/dhtmlxgantt_current_date_line.js
@@ -1,5 +1,5 @@
 gantt.templates.scale_cell_class = function(date){
-    var time_now = new Date("2014, 04, 16");
+    var time_now = new Date();
     var clean_dates = date.setHours(0, 0, 0, 0);
 
     if( clean_dates == time_now.setHours(0, 0, 0, 0)){
