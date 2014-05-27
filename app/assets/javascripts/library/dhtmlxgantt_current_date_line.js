d20103e89df6e512956168c47c063b5f4528bea7
levtolstoi <light.n.night@gmail.com>
2014-05-27 20:40:12 +0400

    Add design on task page

--- app/assets/javascripts/library/dhtmlxgantt_current_date_line.js
+++ app/assets/javascripts/library/dhtmlxgantt_current_date_line.js
@@ -1,6 +1,6 @@
 gantt.templates.scale_cell_class = function(date){
     //gantt-timeline
-    var time_now = new Date("2014, 05, 9");
+    var time_now = new Date();
     var clean_dates = date.setHours(0, 0, 0, 0);
 
     if( clean_dates == time_now.setHours(0, 0, 0, 0)){
