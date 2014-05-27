3cb331cb54b52f75ed847b2ae809585612a98797
levtolstoi <light.n.night@gmail.com>
2014-05-27 14:20:02 +0400

    Add status color

--- app/decorators/tasks/state_decorator.rb
+++ app/decorators/tasks/state_decorator.rb
@@ -3,10 +3,32 @@ module Tasks
     delegate_all
     decorates :task
 
+    def state
+      h.content_tag :span, I18n.t("activerecord.tasks/task.state.#{object.current_state.to_s}"), class: css_class_for_current_state
+    end
+
+    private
+
     # def translates_to_state(scope, state)
     #   prefix = accountable.class.to_s.underscore
     #   I18n.t("activerecord.task.#{prefix}.#{scope}.#{state}")
     # end
+    def css_class_for_current_state
+      css_class_for object.current_state.to_s
+    end
+
+    def css_class_for state
+      css_class = 'primary'
+      css_class = case current_state.to_sym
+                    when :formulated then 'label-warning label'
+                    when :activated then 'label-info label'
+                    when :cancelled then 'label-primary label'
+                    when :executed then 'label-success label'
+                    when :paused then 'label-default label'
+                    else 'label-default label'
+                  end
+      css_class
+    end
 
   end
 end
