dac2c3d80376dd7403b63df740008d373cf8581d
tagirshine <tagirr1@yandex.ru>
2014-05-29 17:30:41 +0400

    edit task controller

--- app/controllers/tasks/tasks_controller.rb
+++ app/controllers/tasks/tasks_controller.rb
@@ -1,6 +1,8 @@
 class Tasks::TasksController < ResourceController
   layout 'base'
 
+  before_filter :subtask, only: [:show]
+
   has_scope :per, default: 10, only: [:index]
   has_scope :for_organization, only: [:index]
 
@@ -16,8 +18,6 @@ class Tasks::TasksController < ResourceController
     @task = Tasks::Task.find(params[:id])
     @task.clear_notifications for: current_user
 
-    @newtask = Tasks::Task.new(:parent_id => @task.id )
-
     super
   end
 
@@ -30,6 +30,7 @@ class Tasks::TasksController < ResourceController
   def create
     @task = Tasks::Task.new(params[:task]).tap do |task|
       task.organization = current_user.organization
+      task.inspector = current_user
     end
 
     @task.updated_by = current_user.id
@@ -39,7 +40,7 @@ class Tasks::TasksController < ResourceController
   def update
     @task = Tasks::Task.find(params[:id])
     @task.organization ||= current_user.organization
-
+    @task.inspector = current_user
     @task.updated_by = current_user.id
 
     super
@@ -59,18 +60,22 @@ class Tasks::TasksController < ResourceController
     task_path(resource)
   end
 
-  def mapped_resource
-    {
-        id: resource.id,
-        title: resource.title,
-        description: resource.text,
-        start_date: resource.started_at.try(:localtime).try(:strftime, "%d-%m-%Y"),
-        duration: days_duration_for(resource)
-    }.to_json.html_safe
+  def subtask
+    @subtask ||= Tasks::Task.new(parent: resource)
   end
 
-  def days_duration_for(task)
-    (task.finished_at - task.started_at).to_i/1.day if task.started_at && task.finished_at
-  end
+  #def mapped_resource
+  #  {
+  #      id: resource.id,
+  #      title: resource.title,
+  #      description: resource.text,
+  #      start_date: resource.started_at.try(:localtime).try(:strftime, "%d-%m-%Y"),
+  #      duration: days_duration_for(resource)
+  #  }.to_json.html_safe
+  #end
+  #
+  #def days_duration_for(task)
+  #  (task.finished_at - task.started_at).to_i/1.day if task.started_at && task.finished_at
+  #end
 
 end
