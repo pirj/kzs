c4d80699e5b26ee56a86a66e55d58144ce6555e4
justvitalius <justvitalius@gmail.com>
2014-06-04 21:43:46 +0400

    Add helper to subscribe to private channel.

---
+++ app/helpers/notifications_helper.rb
@@ -0,0 +1,12 @@
+module NotificationsHelper
+
+  # создает закрытый канал между пользователем и сервером
+  def subscribe_to_sidebar_notifications
+    subscribe_to("/notifications/update/#{current_user.id}")+
+    javascript_tag do
+      raw "PrivatePub.subscribe('/notifications/update/#{current_user.id}', function(data, channel){ new NotificationView(data) })"
+    end
+  end
+
+
+end
