f99cc83fe8b99417d7714928fc729617cb367dc7
justvitalius <justvitalius@gmail.com>
2014-06-09 11:29:08 +0400

    Add 3 modules mock.

--- app/controllers/mock_modules_controller.rb
+++ app/controllers/mock_modules_controller.rb
@@ -1,7 +1,21 @@
 class MockModulesController < ApplicationController
   layout 'base'
-  %w(drive guard dispatcher storehouse maintenance maneuvers settings education administration im objects profile important_traffic news).each do |name|
-    define_method name do
+  %w(drive
+    guard
+    dispatcher
+    storehouse
+    maintenance
+    maneuvers
+    settings
+    education
+    administration
+    im
+    objects
+    profile
+    important_traffic
+    news
+    meteo).each do |name|
+      define_method name do
     end
   end
 
