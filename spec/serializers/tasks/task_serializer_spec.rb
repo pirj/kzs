0abaf8adc84ae4c3261b90ae6e3b7b32c4b1fef4
Vladimir Krivchenko <vladimir.krivchenko@gmail.com>
2014-07-01 16:20:40 +0400

    Fix task serializer specs.

--- spec/serializers/tasks/task_serializer_spec.rb
+++ spec/serializers/tasks/task_serializer_spec.rb
@@ -2,15 +2,17 @@ require 'spec_helper'
 
 describe Tasks::TaskSerializer do
 
+  let(:scope_user) {FactoryGirl.create(:user)}
+
   context 'Tasks API serializer' do
 
     let(:time_freeze) { "2014-05-27 14:50:09 +0400" }
 
     let(:task) { Tasks::Task.new(title: 'Task One', text: 'Desc', started_at: nil, finished_at: nil ) }
 
-    subject { Tasks::TaskSerializer.new(task) }
+    subject { Tasks::TaskSerializer.new(task, scope: scope_user) }
 
-    it('Should have base keys and a root key') { expect(subject.to_json).to eql('{"task":{"id":null,"title":"Task One","started_at":null,"finished_at":null,"parent_id":null,"state":null,"description":"Desc","start_date":null,"duration":0,"executor":null,"inspector":null,"checklists":[]}}')}
+    it('Should have base keys and a root key') { expect(subject.to_json).to eql('{"task":{"id":null,"title":"Task One","started_at":null,"finished_at":null,"parent_id":null,"parent":null,"state":"formulated","actions":["cancel","start"],"description":"Desc","start_date":null,"duration":1,"has_subtasks":false,"has_notification":false,"expired":false,"notifications_count":0,"executor":null,"inspector":null,"checklists":[],"notifications":[]}}')}
 
     context 'Check for user serialization' do
       let(:executor) { build :user, first_name: 'Sam', last_name: 'Serious'}
@@ -23,7 +25,7 @@ describe Tasks::TaskSerializer do
         end
       end
 
-      subject { JSON.parse(Tasks::TaskSerializer.new(task).to_json) }
+      subject { JSON.parse(Tasks::TaskSerializer.new(task, scope: scope_user).to_json) }
 
       it "JSON should contain executor and inspector first_name_and_last_name" do
         expect(subject).to have_key "task"
